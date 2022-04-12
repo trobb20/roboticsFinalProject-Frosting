5363a565ac0a638e112f0be59a99a40328c9054f
CHenley112 <103452569+CHenley112@users.noreply.github.com>
2022-04-12 13:24:50 -0400

    Update frosting_main.py

--- mech/frosting_main.py
+++ mech/frosting_main.py
@@ -13,6 +13,22 @@
 from frosting_board import FrostingMainBoard
 import numpy as np
 
+def split_shapes(commands: list, cutoff_distance: int):
+    shapes = []
+    shapes.append([commands[0]])
+    shape_iteration = 0
+
+    for i in range(len(commands)-1):
+        distance = math.sqrt((commands[i+1][1]-commands[i][1])**2 + (commands[i+1][0]-commands[i][0])**2)
+        if distance > cutoff_distance:
+            shape_iteration += 1
+            shapes.append([])
+
+        shapes[shape_iteration].append(commands[i+1])    
+    
+    return shapes
+
+
 def main():
     main_board = FrostingMainBoard()
                     #    X  Y  E

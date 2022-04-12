cd881a51b5a0b491fe912f44eef41b8817fd6178
Teddy <teddyrobbins3@gmail.com>
2022-04-12 13:34:05 -0400

    testing stepper movement code

--- mech/frosting_main.py
+++ mech/frosting_main.py
@@ -13,33 +13,43 @@
 from frosting_board import FrostingMainBoard
 import numpy as np
 
+
 def split_shapes(commands: list, cutoff_distance: int):
     shapes = []
     shapes.append([commands[0]])
     shape_iteration = 0
 
-    for i in range(len(commands)-1):
-        distance = math.sqrt((commands[i+1][1]-commands[i][1])**2 + (commands[i+1][0]-commands[i][0])**2)
+    for i in range(len(commands) - 1):
+        distance = np.sqrt((commands[i + 1][1] - commands[i][1]) ** 2 + (commands[i + 1][0] - commands[i][0]) ** 2)
         if distance > cutoff_distance:
             shape_iteration += 1
             shapes.append([])
 
-        shapes[shape_iteration].append(commands[i+1])    
-    
+        shapes[shape_iteration].append(commands[i + 1])
+
     return shapes
 
 
 def main():
     main_board = FrostingMainBoard()
-                    #    X  Y  E
-    drawing = np.array(([0, 0, 0],
-                    [50, 0, 1],
-                    [50, 50, 0.7],
-                    [0, 50, 0],
-                    [0, 0, 0]))
+    #    X  Y  E
+    # drawing = np.array(([0, 0, 0],
+    #                     [50, 0, 1],
+    #                     [50, 50, 0.7],
+    #                     [0, 50, 0],
+    #                     [0, 0, 0]))
+
+    theta = np.linspace(0.1, 1 * np.pi, 50)
+    r = 10 * theta
+    X = r * np.cos(theta)
+    Y = r * np.sin(theta)
+    E = np.zeros(np.size(X))
+
+    drawing = np.array(([X, Y, E])).T
 
     main_board.draw(drawing, int(input('Speed: ')), main_board.white_extruder)
     return
+
 
 if __name__ == '__main__':
     main()
